28738955352de95041b55af9ecc6a620f1b51d74
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-08 12:54:06 +0900

    feat: li에 인덱스 데이터 추가

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import $ from './utils/selector.js';
+const listArray = [];
 
-const createNewItem = (value) => {
+const createNewItem = (value, index) => {
   const li = document.createElement('li');
+  li.setAttribute('data-set', index);
+
   const div = document.createElement('div');
   div.classList.add('view');
+
   const input = document.createElement('input');
   input.classList.add('toggle');
   input.setAttribute('type', 'checkbox');
+
   const label = document.createElement('label');
   label.classList.add('label');
   label.textContent = value;
+
   const button = document.createElement('button');
   button.classList.add('destroy');
 
@@ -22,7 +28,8 @@ const handleSubmit = (e) => {
   e.preventDefault();
 
   const $newInput = $('#new-todo-title');
-  createNewItem($newInput.value);
+  createNewItem($newInput.value, listArray.length);
+  listArray.push($newInput.value);
   $newInput.value = '';
 };
 
