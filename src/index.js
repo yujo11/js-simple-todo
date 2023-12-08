cb8e2b83336f74d46e038932c2dad5c2b5eccc94
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-08 12:26:37 +0900

    feat: 새로운 할일 생성하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import $ from './utils/selector.js';
 
-console.log($('.todoapp'));
+const createNewItem = (value) => {
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.classList.add('view');
+  const input = document.createElement('input');
+  input.classList.add('toggle');
+  input.setAttribute('type', 'checkbox');
+  const label = document.createElement('label');
+  label.classList.add('label');
+  label.textContent = value;
+  const button = document.createElement('button');
+  button.classList.add('destroy');
+
+  div.append(input, label, button);
+  li.append(div);
+  $('.todo-list').append(li);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const $newInput = $('#new-todo-title');
+  createNewItem($newInput.value);
+  $newInput.value = '';
+};
+
+$('.todo-form').addEventListener('submit', handleSubmit);
